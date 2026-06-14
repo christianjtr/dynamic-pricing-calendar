e270428296a223208b87eae7edb2ed5041cdf788
Christian José Torrealba Rondón <13179028+christianjtr@users.noreply.github.com>
2026-06-14 21:25:17 +0200

    chore: Add project configs

--- app.config.mjs
+++ app.config.mjs
@@ -2,19 +2,31 @@ import reactRefresh from "@vitejs/plugin-react";
 import { createApp } from "vinxi";
 
 export default createApp({
-  routers: [
-    {
-      name: "client",
-      type: "spa",
-      handler: "./index.html",
-      target: "browser",
-      plugins: () => [reactRefresh()],
-    },
-    {
-      name: "api",
-      type: "http",
-      base: "/api",
-      handler: "./api/index.ts",
-    },
-  ],
+	routers: [
+		{
+			name: "client",
+			type: "spa",
+			handler: "./index.html",
+			target: "browser",
+			plugins: () => [
+				reactRefresh(),
+				{
+					name: "custom-vite-aliases",
+					vite: {
+						resolve: {
+							alias: {
+								"@": "/client",
+							},
+						},
+					},
+				}
+			],
+		},
+		{
+			name: "api",
+			type: "http",
+			base: "/api",
+			handler: "./api/index.ts",
+		},
+	],
 });
---
+++ nitro.config.ts
@@ -0,0 +1,3 @@
+export default {
+	compatibilityDate: new Date().toISOString().split("T")[0],
+};
